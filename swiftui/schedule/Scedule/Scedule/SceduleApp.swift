9766f8ef97e23c172c1a08b40c360e19b5879058
Dmitry Feld <dima@feld.co>
2021-08-06 23:19:13 -0700

    Schedule Empty Project Added

---
+++ swiftui/schedule/Scedule/Scedule/SceduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceduleApp.swift
+//  Scedule
+//
+//  Created by Dmitry Feld on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SceduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
